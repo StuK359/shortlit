45d6cd9fb65f2c6356ea3a129f15abae5ef60aab
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-04-12 14:43:37 -0400

    Add story model

--- shortlit/main_app/models.py
+++ shortlit/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Story(models.Model):
+  title = models.CharField(max_length=50)
+  author = models.CharField(max_length=50)
+  user = models.CharField(max_length=50)
+  genre = models.CharField()
+  date = models.DateField()
+  content = models.TextField()
+  synopsis = models.TextField()
+  cover = models.ImageField()
