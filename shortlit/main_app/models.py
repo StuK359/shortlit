028e3a326a8973e594c1102fcc3076e57377aefd
Stuart Klingman <stuartk@avatek.com>
2021-04-12 15:21:57 -0700

    Stories Index page, migrate model.

--- shortlit/main_app/models.py
+++ shortlit/main_app/models.py
@@ -5,8 +5,9 @@ class Story(models.Model):
   title = models.CharField(max_length=50)
   author = models.CharField(max_length=50)
   user = models.CharField(max_length=50)
-  genre = models.CharField()
+  genre = models.CharField(max_length=50)
   date = models.DateField()
   content = models.TextField()
   synopsis = models.TextField()
-  cover = models.ImageField()
+  cover = models.TextField()
+
--- shortlit/main_app/views.py
+++ shortlit/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Story
 
 # Add the following import
 from django.http import HttpResponse
@@ -11,6 +12,7 @@ def about(request):
     return render(request, 'about.html')
 
 def stories_index(request):
+    stories = Story.objects.all()
     return render(request, 'stories/stories_index.html', {
-        'stories': stories
+            'stories': stories
     })
