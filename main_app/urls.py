8cb11aa8feff282d685090df9739d8164782b64e
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-04-13 14:28:29 -0400

    Create delete view function

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('stories/<int:story_id>/', views.stories_detail, name="detail"),
     path('stories/create/', views.StoryCreate.as_view(), name='stories_create'),
     path('stories/<int:pk>/update/', views.StoryUpdate.as_view(), name='stories_update'),
+    path('stories/<int:pk>/delete/', views.StoryDelete.as_view(), name='stories_delete'),
 ]
