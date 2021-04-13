2f8bce1860e1a2924fd24be0a7d93b61d8db687a
Danielle Martinez <daniellealexandria10@gmail.com>
2021-04-13 15:35:54 -0600

    Create edit and delete functionality and add Review model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.urls import reverse 
 
+
+RATING = (
+  ('1', '1'),
+  ('2', '2'),
+  ('3', '3'),
+  ('4', '4'),
+  ('5', '5'),
+)
+
 # Create your models here.
 class Story(models.Model):
   title = models.CharField(max_length=50)
@@ -15,3 +24,22 @@ class Story(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'story_id': self.id})
+
+
+class Review(models.Model):
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  content = models.CharField(max_length=250)
+  rating = models.CharField(
+    max_length=1,
+    choices=RATING,
+    default=RATING[-1][-1]
+  )
+
+  story = models.ForeignKey(Story, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f" Rating: {self.rating}"
+
+  class Meta:
+    ordering = ['-id']
